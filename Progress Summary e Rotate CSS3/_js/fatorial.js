c0618a9490a64ae9a0de0c1569f8a883d925083b
Luan Junior <luan.adventista@hotmail.com>
2017-06-22 03:02:36 -0300

    Progress, Summary and Rotate Example with CSS3 and JavaScript

---
+++ Progress Summary e Rotate CSS3/_js/fatorial.js
@@ -0,0 +1,34 @@
+﻿
+
+
+function fatorial(n){
+	if(n==0){
+		return 1;
+	}else{
+		var fat = 1;
+		for(var i=1;i<=n;i++){
+			fat = fat * 1;
+		}
+		return fat;
+	}
+}
+
+
+
+function fatorial(n){
+	
+	if(n==0){
+		return 1;
+	}else{
+		return n * fatorial(n-1)
+	}
+}
+	
+	function fatorial(n){
+	
+	if(n==0){
+		return 1;
+	}else{
+		return n * arguments.callee(n-1)
+	}
+	}
